780382f87e20637c9204b94597a595c216086225
Alek30k <alemank23k@gmail.com>
2022-05-24 19:22:30 -0300

    Finish HW 06

--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -1,5 +1,6 @@
 const session = require('supertest-session');
 const app = require('../index.js'); // Importo el archivo de entrada del server de express.
+const { sumArray, pluck } = require('../utils');
 
 const agent = session(app);
 
@@ -16,7 +17,7 @@ describe('Test de APIS', () => {
     it('responds with 200', () => agent.get('/test').expect(200));
     it('responds with and object with message `test`', () =>
       agent.get('/test').then((res) => {
-        expect(res.body.message).toEqual('hola');
+        expect(res.body.message).toEqual('test');
       }));
   });
 
@@ -29,9 +30,16 @@ describe('Test de APIS', () => {
           expect(res.body.result).toEqual(5);
         })
     );
+    it('responds with the sum of 2 and 4', () =>
+      agent.post('/sum')
+        .send({a: 2, b: 4})
+        .then((res) => {
+          expect(res.body.result).toEqual(6);
+        })
+    );
   });
 
-  describe('POST /producto', () => {
+  describe('POST /product', () => {
     it('responds with 200', () => agent.post('/product').expect(200));
     it('responds with the product of 2 and 3', () =>
       agent.post('/product')
@@ -42,15 +50,67 @@ describe('Test de APIS', () => {
     );
   });
 
+  describe('function sumArray', ()=>{
+    const arr = [1,2,3,4]
+    it('si encuentra la suma retorna true', ()=>{
+      expect(sumArray(arr, 5)).toBe(true)
+    })
+    it('si no encuentra la suma retorna false', ()=>{
+      expect(sumArray(arr, 85)).toBe(false)
+    })
+    it('si no le paso un array como primer parametro arroja un error', ()=>{
+      expect(() => sumArray(1, 85)).tothrow(TypeError)
+    })
+    it('no deberia sumar dos veces el mismo numero', ()=>{
+      expect(sumArray(arr, 2)).toBe(false)
+    })
+  })
+
   describe('POST /sumArray', () => {
-    it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
-      agent.post('/sumArray')
+    it('responde con 400 si no le envio los datos correctos', () => agent.post('/sumArray').expect(400));
+    it('responds with 200', () => agent.post('/sumArray').send({array: [2,5,7,10,11,15,20], num: 13}).expect(200));
+
+    it('devuelve true si la suma de alguno de los numeros del array es igual a num', () =>
+       agent.post('/sumArray')
         .send({array: [2,5,7,10,11,15,20], num: 13})
         .then((res) => {
           expect(res.body.result).toEqual(true);
       }));
   });
+
+  describe('POST a /numString', ()=>{
+    it('responds with 200', () => agent.post('/numString').send({word: 'hola'}).expect(200));
+    it('responde con 400 si el string es un numero', () => agent.post('/numString').send({word: 1}).expect(400));
+    it('responde con 400 si el string esta vacio', () => agent.post('/numString').send({word: ''}).expect(400));
+
+    
+    it('Si le mando hola que devuelva 4', ()=>{
+      agent.post('/numString')
+      .send({word: 'hola'})
+      .then(res => {
+        expect(res.body.result).toBe(4)
+      })
+    })
+  })
+
+  describe('function pluck', ()=>{
+    const arr = [{nombre: "Ale", apellido: "Cabrera"}, {nombre: "Juan", apellido: "Perez"}]
+    it('retorna un array con solo nombres', ()=>{
+      expect(pluck(arr, 'nombre')).toEqual(['Ale', 'Juan'])
+    })
+  })
+
+  describe('POST /pluck', ()=>{
+    const arr = [{nombre: "Ale", apellido: "Cabrera"}, {nombre: "Juan", apellido: "Perez"}]
+    it('responds with 200', () => agent.post('/pluck').send({array: arr, prop: 'nombre'}).expect(200));
+    it ('si le mando el array que me devuelva los nombres', ()=>{
+      agent.post('/pluck')
+      .send({array: arr, prop: 'nombre'})
+      .then((res)=>{
+        espect(res.body.result).toEqual(['Ale', 'Juan'])
+      })
+    })
+  })
 
 });
 
---
+++ 06-Testing/homework/utils.js
@@ -0,0 +1,23 @@
+const { arrayReplaceAt } = require("markdown-it/lib/common/utils")
+
+function sumArray(array, num){
+    if(!array.isArray(array)) throw TypeError('array')
+    if(typeof num !== 'number') throw TypeError('num')
+
+    for(var i = 0; i < array.length; i++) {
+        for(var j = i + 1; j < array.length; j++){
+            if(array[i] + array[j] === num) return true
+        }
+    }
+    return false
+}
+
+//[{nombre: Alejandro, apellido: Cabrera}, {nombre: juan, apellido: Perez}], 'nombre
+function pluck(arr, prop){
+    return arr.map(o => o[prop])
+}
+
+module.exports = {
+    sumArray,
+    pluck
+}
